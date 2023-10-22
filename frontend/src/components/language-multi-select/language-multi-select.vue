84dd3adc94679e91cff9e626aeaa2fae232f454f
Leon <stone92822@outlook.com>
2023-10-22 10:10:00 -0400

    adding a component

---
+++ frontend/src/components/language-multi-select/language-multi-select.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="col-12" :class="{'blur-loading-row': loading}">
+    <multi-select
+      ref="multiSelect"
+      :options="languageOptions"
+      :selected-options="selectedLanguages"
+      :placeholder="placeholder"
+      :is-disabled="isDisabled"
+      :non-removable-values="nonRemovableValues"
+      @restricted-option-removal="() => $emit('restricted-option-removal')"
+      title="Language list"
+      :custom-filtered-options="customFilteredOptions"
+      @click.native="retrieveOptions"
+      @select="onLanguageSelected">
+    </multi-select>
+  </div>
+</template>
+
+<script src="./language-multi-select.js"></script>
