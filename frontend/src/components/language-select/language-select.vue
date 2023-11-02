ff9315fe956cf7a83d6d6d22f6b68eee630d3435
Leon <stone92822@outlook.com>
2023-11-02 10:10:00 -0400

    adding a component

---
+++ frontend/src/components/language-select/language-select.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="containerClass">
+    <simple-basic-select
+      v-model="selected"
+      v-bind="wrappedProps"
+      :disabled="isDisabledSelection"
+      :fetch-on-created="fetchOnCreated"
+      @click.native="onClick"
+      title="Language list"
+      :placeholder="placeholder"
+      :options="languageOptions"
+      :format-option="formatOption"
+      :filter-option="filterOption"
+      :filter-option-context="filterOptionContext"
+      :empty-option="emptyOption"
+      :pre-fetch-option="preFetchOption"
+      :allowSelectedNotInList="allowSelectedNotInList"
+      :non-removable-values="nonRemovableValues"
+      valueKey="isoCode"
+      entity-name="Language"
+      :max-options-timeout="-1"
+      v-on="wrappedListeners"
+    />
+  </div>
+</template>
+
+<script src="./language-select.js"></script>
