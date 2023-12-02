43662b0098041e8e565890aabd681120150284e9
Leon <stone92822@outlook.com>
2023-12-02 10:10:00 -0500

    adding a component

---
+++ frontend/src/components/modal/lms-modal.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="lms-modal" v-if="value">
+    <div class="lms-modal_overlay" @click="dismiss"/>
+    <div class="lms-modal_body" :data-e2e-type="modalDataE2e">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script src="./lms-modal.js" />
+<style src="./lms-modal.scss" lang="scss" />
