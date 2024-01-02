c0f25458fcc43213b373ebdf84f7410c806aef2b
Leon <stone92822@outlook.com>
2024-01-02 10:10:00 -0500

    adding a component

--- frontend/src/components/form/re-captcha.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <label class="hidden" for="g-recaptcha-response">Re captcha</label>
-    <div id="g_recaptcha" class="g_recaptcha" data-sitekey="siteKey"></div>
-  </div>
-</template>
-
-<script src="./re-captcha.js"></script>
--- frontend/src/components/form/utc-flatpickr.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <flat-pickr
-    :disabled="triggeredDisabled"
-    v-bind="wrappedProps"
-    v-on="wrappedListeners"
-    v-model="clientDateStr"></flat-pickr>
-</template>
-
-<script src="./utc-flatpickr.js"></script>
