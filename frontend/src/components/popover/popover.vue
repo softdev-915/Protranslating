8eef7a7024ebaf2dce8c66d8f5ddd8e131935123
Leon <stone92822@outlook.com>
2024-01-12 10:10:00 -0500

    adding a component

---
+++ frontend/src/components/popover/popover.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="popover" :style="popoverStyle" v-show="showPopover" ref="popover">
+    <slot></slot>
+  </div>
+</template>
+
+<script src="./popover.js"></script>
+<style scoped lang="scss" src="./popover.scss"></style>
