0bb5644ac7cc16b533ff3bfcb9b12e52edea77a1
Leon <stone92822@outlook.com>
2024-01-22 10:10:00 -0500

    adding a component

---
+++ frontend/src/components/form/phone-input.vue
@@ -0,0 +1,10 @@
+<template>
+  <input
+    v-bind="customProps"
+    v-on="customListeners"
+    class="form-control"
+    type="text"
+    v-model="phone">
+</template>
+
+<script src="./phone-input.js"></script>
