00f0acf1c20a2d85c678494f71d2280e5ee389a9
Leon <stone92822@outlook.com>
2024-02-04 10:10:00 -0500

    adding a component

---
+++ frontend/src/components/form/pts-email-input.vue
@@ -0,0 +1,18 @@
+<template>
+  <input ref="email"
+    v-on="wrappedListeners"
+    aria-label="User email"
+    :id="elemId"
+    :autocomplete="autocomplete"
+    :name="elemName"
+    type="email"
+    v-model="email"
+    :placeholder="placeholder"
+    :class="classes"
+    :style="styles"
+    @keyup="keyup"
+    @keydown="keydown"
+    @keypressed="keypressed">
+</template>
+
+<script src="./pts-email-input.js"></script>
