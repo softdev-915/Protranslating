5734f268380d232d83550db3a0b4ef346605528b
Leon <stone92822@outlook.com>
2024-04-02 10:10:00 -0400

    adding a component

---
+++ frontend/src/components/request-select/request-ajax-multi-select.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <ajax-multi-select
+    ref="abs"
+    v-bind="wrappedProps"
+    v-on="wrappedListeners"
+    :placeholder="placeholderMessage"
+    :isDisabled="isSelectDisabled"
+    loading-icon-class="fas fa-spinner fa-pulse fa-fw"
+    :class="{'blur-loading-row' : loading}"
+    :http-client="httpClient">
+  </ajax-multi-select>
+</template>
+
+<script src="./request-ajax-multi-select.js"></script>
