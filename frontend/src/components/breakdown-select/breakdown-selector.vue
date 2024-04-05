7c075eae909daf810ab55ee2c62cf1680ec847a0
Leon <stone92822@outlook.com>
2024-04-05 10:10:00 -0400

    adding a component

---
+++ frontend/src/components/breakdown-select/breakdown-selector.vue
@@ -0,0 +1,12 @@
+<template>
+  <simple-basic-select
+    v-model="selected"
+    :options="options"
+    :format-option="formatOption"
+    @click.native="retrieveOptions"
+    entity-name="Breakdown"
+    v-bind="wrappedProps"
+    v-on="wrappedListeners"/>
+</template>
+
+<script src="./breakdown-selector.js"></script>
