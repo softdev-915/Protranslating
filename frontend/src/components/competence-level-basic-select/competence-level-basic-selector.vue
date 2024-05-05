bfd923c2fefd31ffed2ce6830a8c704d1c235b8c
Leon <stone92822@outlook.com>
2024-05-05 10:10:00 -0400

    adding a component

---
+++ frontend/src/components/competence-level-basic-select/competence-level-basic-selector.vue
@@ -0,0 +1,13 @@
+<template>
+  <simple-basic-select
+    v-model="selected"
+    v-bind="wrappedProps"
+    :options="options"
+    :format-option="formatOption"
+    :fetch-on-created="false"
+    @click.native="retrieveOptions"
+    entity-name="Competence Level"
+    v-on="wrappedListeners"/>
+</template>
+
+<script src="./competence-level-basic-selector.js"></script>
