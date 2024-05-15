78a9da6cc52b9043cf6ea0954004f2a74b510361
Leon <stone92822@outlook.com>
2024-05-15 10:10:00 -0400

    adding a component

---
+++ frontend/src/components/competence-level-select/competence-level-selector.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="containerClass">
+    <multi-select
+      :selected-options="selectedOption"
+      :options="competenceLevelOptions"
+      :class="{'blur-loading-row': loading}"
+      @click.native="retrieveOptions"
+      @select="onCompetenceLevelSelected"
+      v-bind="wrappedProps"
+      v-on="wrappedListeners">
+    </multi-select>
+  </div>
+</template>
+
+<script src="./competence-level-selector.js"></script>
