19335ee12031dec061b69f1b322313555d8253b1
Leon <stone92822@outlook.com>
2024-06-22 10:10:00 -0400

    adding a component

---
+++ frontend/src/components/time-to-deliver-select/time-to-deliver-select.vue
@@ -0,0 +1,16 @@
+<template>
+  <simple-basic-select
+    v-model="selected"
+    placeholder="Time to deliver"
+    :options="options"
+    :format-option="formatOption"
+    :filter-option="filterOption"
+    @click.native="_retrieve"
+    entity-name="TimeToDeliver"
+    @options-loaded="onCurrencyListLoaded"
+    :fetch-on-created="fetchOnCreated"
+    v-bind="wrappedProps"
+    v-on="wrappedListeners"/>
+</template>
+
+<script src="./time-to-deliver-select.js"></script>
