bc92548eda06b19ea378f7cc6a98bbc48958d491
Leon <stone92822@outlook.com>
2024-07-15 10:10:00 -0400

    adding a component

---
+++ frontend/src/components/file-upload/file-upload.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>
+    <span @click="onFileUpload" data-e2e-type="file-upload-button-container">
+      <slot name="file-upload-button" />
+    </span>
+    <input
+      type="file"
+      ref="fileInput"
+      data-e2e-type="file-upload-input"
+      @change="onFileInputChanged"
+      :hidden="hasFileUploadButtonSlot"
+      :disabled="disabled"
+      :accept="accept"
+    />
+  </span>
+</template>
+<script src="./index.js"></script>
