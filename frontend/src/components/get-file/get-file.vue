a3528e083a1c093165bfb487b875444f9970f632
Leon <stone92822@outlook.com>
2024-08-05 10:10:00 -0400

    adding a component

---
+++ frontend/src/components/get-file/get-file.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="row align-items-center pt-5">
+      <iframe-download
+        v-if="!noIframe"
+       :url="fileUrl"
+       :downloadOnCreate="true"
+        @download-error="onDownloadError"
+        @download-finished="onDownloadFinished" />
+      <div class="col-12 text-center">
+        <h4>{{ message }} <a v-show="noIframe" :ref="'downloadLink'">here</a></h4>
+      </div>
+    </div>
+    <div class="row" v-if="hasError">
+      <div class="col-12 text-center red-text">
+        <h5>{{ error }}</h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./get-file.js"></script>
